0f9980b1bd9bb4c301ac57cbadcba58c61d65105
gogikarsandeep10-s <gogikarsandeep10@gmail.com>
2026-08-15 21:16:49 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ['askmynotes-io6i.onrender.com']
-  }
-})
