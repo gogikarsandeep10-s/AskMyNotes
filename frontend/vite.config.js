0ff47d945e44bd43c2bf9584041af4cd70242554
gogikarsandeep10-s <gogikarsandeep10@gmail.com>
2026-08-17 20:51:45 +0530

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["askmynotes-frontend-pcl7.onrender.com"],
+    allowedHosts: ["askmynotes-web.onrender.com"],
   },
 });
